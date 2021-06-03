b9724f853a0ff806ca72627e974765ff050084d8
ryo5515 <ryo.miyawaki5515@gmail.com>
2021-06-04 00:12:57 +0900

    フォロー機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :articles
   has_many :likes
-  validates :account_id, :account_name, presence: true
+  has_many :follows
+  validates :account_id, :account_name, presence: true 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_142109) do
+ActiveRecord::Schema.define(version: 2021_06_03_150504) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "tweet", null: false
