f06d22c45b390d858995c11969692912de8a0fa8
ryo5515 <ryo.miyawaki5515@gmail.com>
2021-06-04 22:42:12 +0900

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :likes
   has_many :notifications
-  validates :tweet, presence: true, length: { maximum: 20 }
+  validates :tweet, presence: true, length: { maximum: 30 }
 end
